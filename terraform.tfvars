e6c3291900cd22f88278d96368ebe7b2645f409e
Muhammad Hasnain <mhasnain@Muhammads-MacBook-Air.local>
2022-09-08 22:10:38 -0400

    first

---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+instancetype = "t2.micro"
+
+ami = "ami-08d2d8b00f270d03b"	
+
+key = "mo"
+
+cidr = "10.0.0.0/16"
+
+bucket = "alpha-bucket-16"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "instancetype" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "key" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "bucket" {
+  type = string
+}
